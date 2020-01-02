32d71a10ccb71edc05939d6eca8d592a3b591c28
TaeKyoungKim <moduedu@gmail.com>
2020-01-02 13:18:25 +0900

    make coding at AM 2020/01/02

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var listRouter = require('./router/index')
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine' , 'ejs');
 
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended:false}));
 
--- router/index.js
+++ router/index.js
@@ -4,17 +4,17 @@ var mysql = require('mysql')
 require('dotenv').config()
 
 var db = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PW,
-    database:process.env.DB_NAME
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    database:'o2'
 })
 
 router.get('/topic/add',(req, res)=>{
     var sql = 'SELECT * FROM topic'
     db.query(sql,(err, result)=>{
         if(!err){
-        // console.log(result)
+        console.log(result)
         res.render('add',{topics:result})
     } else {
         console.log(err)
@@ -34,7 +34,7 @@ router.post('/topic/add' ,(req, res)=>{
     db.query(sql,params,(err, result)=>{
         if(!err){
             console.log("성공적으로 저장되었습니다.")
-            res.redirect('/topic/add')
+            res.redirect(`/topic/${result.insertId}`)
         } else {
             console.log(err)
         }
@@ -42,17 +42,24 @@ router.post('/topic/add' ,(req, res)=>{
 
 })
 
-router.get('/topic/:id' ,(req ,res)=>{
-    console.log(req.params.id);
-    var id = req.params.id
-    var sql = 'SELECT * FROM topic WHERE id=?'
-    db.query(sql,[id],(err, result)=>{
-        if(!err){
-            console.log(result)
-            res.render('test',{topic:result})
+router.get(['/topic','/topic/:id'] ,(req ,res)=>{
+    var sql = "SELECT * FROM topic";
+    db.query(sql, (err , results)=>{
+        var id = req.params.id
+        if(id){
+            var sql = 'SELECT * FROM topic WHERE id=?'
+            db.query(sql,[id],(err , result)=>{
+            if(!err){
+                res.render('view' ,{topics: results, topic:result[0]})
+            }else {
+                console.log(err);
+                res.status(500).send("Internal Server Error")
+            }
+        })
         } else {
-            console.log(err)
+            res.render('view', {topics:results , topic:undefined})
         }
+        
     })
 })
 
